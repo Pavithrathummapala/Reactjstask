6ffd0f270b4ece8359b666fad939ed76bfc9d7c2
Pavithrathummapala <21a91a04p5@aec.edu.in>
2023-09-25 23:14:22 +0530

    tag

---
+++ Components/QuestionBank.js
@@ -0,0 +1,59 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is ReactJs?",
+        options: ["Server-side framework", "user interface framework", "both a and b", "none"],
+        answer: "user interface framework",
+    },
+    {
+        id: 2,
+        question: "Who  create React.js?",
+        options: ["Jordan Mike", "Jordan walke", "tim Lee", "Jordan Lee"],
+        answer: "Jordan walke",
+    },
+    {
+        id: 3,
+        question: "In which language is React.js written",
+        options: ["python", "Javascript", "Java", "PHP"],
+        answer: "Javascript"
+    },
+    {
+        id: 4,
+        question: "In MVC, what does React.js act as?",
+        options: ["Model", "Controller", "View", "Middleware"],
+        answer: "View"
+    },
+    {
+        id: 5,
+        question: "Identify the smallest building block of React.JS?",
+        options: ["Props", "Elements", "Components", "None"],
+        answer: "Components"
+    },
+    {
+        id: 6,
+        question: "How many elements does a react component return?",
+        options: ["Elements", "Multiple Elements", "Element", "None"],
+        answer: "Multiple Elements"
+    },
+    {
+        id: 7,
+        question: "Which of the following API is a MUST for every ReactJS component?",
+        options: ["render", "getInitialState", "renderComponent", "None"],
+        answer: "renderComponent"
+    },
+    {
+        id: 8,
+        question: "Which of the following is used in React.js to increase performance?",
+        options: ["Virtual Dom", "Original Dom", "both a and b", "None"],
+        answer: "Virtual Dom"
+    },
+    {
+        id: 9,
+        question: "How many elements can a valid react component return?",
+        options: ["1", "2", "3", "4"],
+        answer: "1"
+    },
+    
+]
+ 
+export default qBank;
